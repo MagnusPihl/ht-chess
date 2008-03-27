db1870aafab91bb1233c15fad85ee37d43f3e283
magnus.pihl <magnus.pihl@afdedcdd-3e49-0410-aff0-259e14133cc1>
2008-03-27 11:56:27 +0000

    General formatting changes.

--- trunk/ht-chess/src/Move.h
+++ trunk/ht-chess/src/Move.h
@@ -1,5 +1,5 @@
-public class Move {
-	
+public class Move
+{
 	private int to;
 	private int from;
 	private Piece special;
@@ -8,37 +8,44 @@ public class Move {
 
 	public Move(int from, int to, Piece special, Piece type, Piece content) :
 		this->from(from), this->to(to), this->special(special), this->type(type), this->content(content)
-	{
-	
-	}
+		{
 
-	//
-	public void execute(Board board) {
-	
-	}
+		}
 
-	public void unexecute(Board board) {
-	
-	}
+		//
+		public void execute(Board board)
+		{
 
-	//accessors
-	public int getOldPosition() {
-		return from;
-	}
+		}
 
-	public int getNewPosition() {
-		return to;
-	}
-	
-	public Piece getSpecial() {
-		return special;
-	}
-	
-	public Piece getPieceType() {
-		return type;
-	}
-	
-	public Piece getContent() {
-		return content;
-	}
+		public void unexecute(Board board)
+		{
+
+		}
+
+		//accessors
+		public int getOldPosition()
+		{
+			return from;
+		}
+
+		public int getNewPosition()
+		{
+			return to;
+		}
+
+		public Piece getSpecial()
+		{
+			return special;
+		}
+
+		public Piece getPieceType()
+		{
+			return type;
+		}
+
+		public Piece getContent()
+		{
+			return content;
+		}
 };
