b2e22317a35d0cb894744c14fddb28bd95ec5acd
lmklmx@gmail.com <lmklmx@gmail.com@afdedcdd-3e49-0410-aff0-259e14133cc1>
2008-04-23 12:35:29 +0000

    Added documentation Added som color define functions Changed color to enum Piece define functions now cast integers to their respective enums

--- ht-chess/src/Piece.h
+++ ht-chess/src/Piece.h
@@ -1,13 +1,45 @@
 #ifndef PIECE_H
 #define PIECE_H
 
-#define BLACK 0x10
-#define WHITE 0x20
+/**
+* Returns true if color is equal to wanted color
+*/
+#define IS_COLOR(color, wantedColor) (((color) & PIECE_COLOR) == (wantedColor))
+
+/**
+* Returns true if color is WHITE
+*/
+#define IS_BLACK(color) (((color) & PIECE_COLOR) == BLACK)
+
+/**
+* Returns true if color is BLACK
+*/
+#define IS_WHITE(color) (((color) & PIECE_COLOR) == WHITE)
+
+/**
+* Bit mask for colors
+*/
 #define PIECE_COLOR (BLACK | WHITE)
+
+/**
+* Bit mask for piece type
+*/
 #define PIECE_TYPE 0x0F
-#define GET_OPPOSITE_COLOR(color) ((color) ^ PIECE_COLOR)
-#define GET_PIECE_COLOR(x) ((x) & PIECE_COLOR)
-#define GET_PIECE_TYPE(x) ((x) & PIECE_TYPE)
+
+/**
+* Returns opposite WHITE if BLACK, BLACK if WHITE
+*/
+#define GET_OPPOSITE_COLOR(color) ((Color)((color) ^ PIECE_COLOR))
+
+/**
+* Returns the color of a piece
+*/
+#define GET_PIECE_COLOR(x) ((Color)((x) & PIECE_COLOR))
+
+/**
+* Returns the piece type of a piece
+*/
+#define GET_PIECE_TYPE(x) ((Piece)((x) & PIECE_TYPE))
 
 static const int PIECE_VALUE[7] = {
 	10000,	//konge
@@ -17,6 +49,13 @@ static const int PIECE_VALUE[7] = {
 	300,	//springer
 	100,	//bonde
 	0,	//intet	
+};
+
+
+enum Color 
+{
+	BLACK = 0x10,
+	WHITE = 0x20
 };
 
 enum Piece
