1c0278c19f27c7967fbeda08a74d4d5e845e6796
magnus.pihl <magnus.pihl@afdedcdd-3e49-0410-aff0-259e14133cc1>
2008-04-23 09:33:05 +0000

    Implemented AlphaBeta algorithm.
    Added detection of endgame situations to Core.

--- trunk/ht-chess/src/MoveSelector.h
+++ trunk/ht-chess/src/MoveSelector.h
@@ -14,7 +14,7 @@ private:
 
 	int miniMax(Board &board, Move &path, bool isMaximizer=true, int curDepth=0, int maxDepth=100)
 	{
-		if(curDepth == maxDepth /* && board.gameEnded() ??? */)		//if leaf
+		if(curDepth == maxDepth || board.isCheckmate() || board.isStalemate())		//if leaf
 		{
 			return 0;//BoardEvaluator.boardValue(board);
 		}
@@ -68,10 +68,57 @@ public:
 
 class AlphaBeta
 {
-public:
-	Move operator()(Board board, int alpha=-10000, int beta=10000,
-		bool isMaximizer=true, int curDepth=0, int maxDepth=100)
+private:
+	MoveGenerator moveGen;
+
+	int alphaBeta(Board &board, Move &path, bool isMaximizer=true, int curDepth=0,
+		int maxDepth=100, int alpha=-100000, int beta=100000)
 	{
+		if(curDepth == maxDepth || board.isCheckmate() || board.isStalemate())		//if leaf
+		{
+			return 0;//BoardEvaluator.boardValue(board);
+		}
+		else if(isMaximizer)	//if maximizer
+		{
+			std::vector<Move> moveList;
+			moveGen.generateMoves(board, WHITE, moveList);
+			for(std::vector<Move>::iterator itr = moveList.begin(); itr != moveList.end() && alpha < beta; itr++)
+			{
+				(*itr).execute(board);
+				int V = alphaBeta(board, path, false, curDepth+1, maxDepth, alpha, beta);
+				if(V > alpha)
+				{
+					alpha = V;
+					if(curDepth==0)	path = (*itr);
+				}
+				(*itr).unexecute(board);
+			}
+			return alpha;
+		}
+		else	//if minimizer
+		{
+			std::vector<Move> moveList;
+			moveGen.generateMoves(board, BLACK, moveList);
+			for(std::vector<Move>::iterator itr = moveList.begin(); itr != moveList.end() && alpha < beta; itr++)
+			{
+				(*itr).execute(board);
+				int V = alphaBeta(board, path, true, curDepth+1, maxDepth, alpha, beta);
+				if(V < beta)
+				{
+					beta = V;
+					if(curDepth==0)	path = (*itr);
+				}
+				(*itr).unexecute(board);
+			}
+			return beta;
+		}
+	}
+public:
+	Move operator()(Board &board, bool isMaximizer=true, int maxDepth=DEFAULT_PLY)
+	{
+		Move path;
+		alphaBeta(board, path, isMaximizer, 0, maxDepth);
+		return path;
 	}
 };
 
