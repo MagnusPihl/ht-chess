195e015f5b4db4e880aeb3f1f72262992e58519a
lmklmx@gmail.com <lmklmx@gmail.com@afdedcdd-3e49-0410-aff0-259e14133cc1>
2008-04-23 09:09:08 +0000

    Seperated MoveGenerator into cpp and h file

---
+++ trunk/ht-chess/src/MoveGenerator.cpp
@@ -0,0 +1,19 @@
+#ifndef MOVE_GENERATOR_CPP
+#define MOVE_GENERATOR_CPP
+
+#include "MoveGenerator.h"
+
+void MoveGenerator::generateMoves(Board &board, int color, std::vector<Move> &moves) {			
+	int x, y, position;		
+	
+	for (x = 0; x < ROW_COUNT; ++x) {
+		for (y = 0; y < COLUMN_COUNT; ++y) {	
+			position = GET_POSITION(x,y);
+			if (GET_PIECE_COLOR(board.getItemAt(position)) == color) {
+				board.getMovesFromPosition(position, moves);
+			}			
+		}
+	}
+}
+
+#endif MOVE_GENERATOR_H
